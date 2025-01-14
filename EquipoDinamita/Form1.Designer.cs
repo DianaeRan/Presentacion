cf61d88b82a434b5707eb964357d1a7caa993540
Victor Perez <vp40y20@gmail.com>
2025-01-13 21:42:21 -0600

    dfxtg

--- EquipoDinamita/Form1.Designer.cs
+++ EquipoDinamita/Form1.Designer.cs
@@ -274,7 +274,7 @@
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Green;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(332, 9);
+            this.label7.Location = new System.Drawing.Point(783, 14);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(393, 32);
             this.label7.TabIndex = 31;
